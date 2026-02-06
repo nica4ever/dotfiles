6d674f78d0b272306ded2410074d00bc800ade1a
Nika/Cojanu Ionut <cojanuionut9@gmail.com>
2026-02-07 00:01:45 +0100

    feat: wallpaper script that changes background after 10 mins / still testing it

---
+++ .config/sway/wlp.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Wallpaper directory
+WP_FOLDER=~/Pictures/Wallpapers/Batch\ 1/
+
+# Time in seconds to change wallpaper
+WAIT_TIME=3599
+
+while true; do
+    PID=$(pidof swaybg)
+    FILE=$(find "$WP_FOLDER" -type f -name '*.png' -o -name '*.jpg' | shuf -n1)
+    swaybg -i "$FILE" -m fill &
+    sleep 1
+    kill "$PID"
+    sleep "$WAIT_TIME"
+done
