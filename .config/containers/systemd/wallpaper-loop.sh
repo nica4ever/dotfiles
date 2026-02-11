49bc7f89ca4e3b853eb1a4ea45457f85891cd951
Nika/Cojanu Ionut <cojanuionut9@gmail.com>
2026-02-11 14:00:11 +0100

    chore: fixed wallpaper config, I could not find a way to bypass the black screen that comes with sway when you first log in

--- .config/containers/systemd/wallpaper-loop.sh
+++ .config/containers/systemd/wallpaper-loop.sh
@@ -1,15 +1,16 @@
 # ~/.config/containers/systemd/wallpaper-loop.sh
 #!/bin/sh
-#!/bin/sh
 until [ -S "$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" ]; do
   sleep 1
 done
 swww-daemon &
 sleep 1
+
 while true; do
   swww img "$(find /wallpapers -type f \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' -o -name '*.webp' \) | shuf -n 1)" \
-    --transition-type fade \
-    --transition-duration 2 \
-    --transition-fps 60
+  --transition-type fade \
+  --transition-duration 3 \
+  --transition-fps 60 \
+  --transition-bezier .42,0,.58,1
   sleep 600
 done
